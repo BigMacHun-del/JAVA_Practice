eb394a7cf9547cfbd56cd8aef3efd97f7a07cc14
김대훈 <eogns136@gmail.com>
2025-12-03 17:35:44 +0900

    add: 배열, 메서드 연습 추가

---
+++ src/chapter1/One_10.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+
+public class One_10 {
+    public static void main(String[] args) {
+        //TODO: 메서드
+        Calculator calculator = new Calculator(); // ✅ Calculator 객체 소환
+        int result = calculator.sum(1, 2); // ✅ 호출부
+        System.out.println("결과: " + result);
+    }
+
+}
